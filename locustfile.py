97871cf2bf204e233dfb2cdb338a64903015b7a2
Hilton Rocha <hiltonrocha@MacBook-Pro-de-Hilton.local>
2023-10-25 13:09:40 -0300

    add load test

---
+++ locustfile.py
@@ -0,0 +1,26 @@
+from locust import HttpUser, task, between
+
+class Healthcheck(HttpUser):
+    wait_time = between(1, 3)  # Wait between 1 and 3 seconds
+
+    @task
+    def health_check(self):
+        self.client.get('/health_check')
+    
+    @task(2)
+    def list_user_by_id(self):
+        self.client.get('/v1/usuario/1')
+
+    @task
+    def list_all_users(self):
+        self.client.get('/v1/usuario/')
+
+    @task
+    def create_user(self):
+        self.client.post('/v1/usuario/criar', json={
+            "nome": "name",
+            "senha": "12345",
+            "cpf": "03477306040",
+              "tipo": "user"
+            })
+        
