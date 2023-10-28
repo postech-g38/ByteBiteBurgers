94f196a3dbc89c28f5103771661e29b64c067a80
Luiz <luiz.f_ferraresi@hotmail.com>
2023-10-28 20:21:10 -0300

    add pedido by status

--- src/adapters/repositories/pedido_repository.py
+++ src/adapters/repositories/pedido_repository.py
@@ -1,9 +1,16 @@
+from sqlalchemy import select
+
 from src.adapters.repositories.sqlalchemy_repository import SQLAlchemyRepository
 from src.adapters.database.models.pedido_model import PedidoModel
 
 
 class PedidoRepository(SQLAlchemyRepository):
-     def __init__(self, database_session) -> None:
+    def __init__(self, database_session) -> None:
         super().__init__(session=database_session)
         self.entity_model = PedidoModel
-        
+    
+    def get_by_status(self, status: str) -> list:
+        stmt = select(self.entity_model).where(self.entity_model.status_pedido == status)
+        results = self.session_db.execute(stmt)
+        return results.scalars().all()
+    
