e50033fc0f09b821b03c70da0ec29b7e7c184c7b
Luiz <luiz.f_ferraresi@hotmail.com>
2023-10-28 21:07:57 -0300

    update delete user

--- src/routes/v1/usuario_route.py
+++ src/routes/v1/usuario_route.py
@@ -66,9 +66,9 @@ def update(
 
 @router.delete(
     path='/{id}', 
-    response_model=int, 
+    # response_model=int, 
     summary='Deletar Usuario'
 )
-def delete(id: int, repository: EntityRepository = Depends()) -> dict:
+def delete(id: str, repository: EntityRepository = Depends()) -> dict:
     service = UsuarioService(repository=repository)
     return service.delete(id=id)
