90f13f898507a46e351b4adba175a8a1503ee88d
Luiz <luiz.f_ferraresi@hotmail.com>
2023-10-29 20:05:54 -0300

    update pedido

--- src/adapters/database/models/pedido_model.py
+++ src/adapters/database/models/pedido_model.py
@@ -11,6 +11,10 @@ from src.adapters.database.models.entity_model import EntityModel
 class PedidoModel(EntityModel):
     __tablename__ = 'pedido'
 
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False, index=True, autoincrement=True)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    updated_at: Mapped[datetime | None] = mapped_column(onupdate=datetime.utcnow)
+    deleted_at: Mapped[datetime | None]
     data_mudanca_status: Mapped[datetime | None]
     valor: Mapped[float]
     status_pedido: Mapped[str | None]
