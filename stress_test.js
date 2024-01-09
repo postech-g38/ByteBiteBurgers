eeec5fc52c3f85d734f71defa61cd2052ce55ee4
Hilton Rocha <hilton-bruno-santos@hotmail.com>
2024-01-09 20:01:25 -0300

    add services

--- stress_test.js
+++ stress_test.js
@@ -2,13 +2,13 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 10,  // 10 Virtual Users
-  interactions: 20, // 20 interactions per Virtual users 10 * 20
-  duration: '40s', // test duration time 40 seconds
+  vus: 20,  // 10 Virtual Users
+  interactions: 50, // 20 interactions per Virtual users 10 * 20
+  duration: '60s', // test duration time 40 seconds
 };
 
 // 10 * 20 = 200 request to get/pedido in 40 seconds
 export default function () {
-  http.get('http://ad284c1ac441545f18154385f507000c-2108317586.us-east-1.elb.amazonaws.com:8000/v1/pedido/');
+  http.get("http://aea830e58c65a4b6ca8f25b3eb251257-1762251680.us-east-1.elb.amazonaws.com:8000/v1/usuario/");
   sleep(1);
 }
