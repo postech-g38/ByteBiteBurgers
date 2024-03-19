39264aa317cfbc7c9bdf3d3b064bd7d90427a58f
Hilton Rocha <hilton-bruno-santos@hotmail.com>
2024-03-19 20:39:08 -0300

    teste

--- db.py
+++ db.py
@@ -5,9 +5,3 @@ if __name__ == '__main__':
     run_migrations()
 
 ##### end
-    
-# export DATABASE_USERNAME='postgres'
-# export DATABASE_PASSWORD='HtA}ID[[PU2aMt~JdJCa_13e1D-!'
-# export DATABASE_HOST='rds-prd.cl6m0ukm6axd.us-west-2.rds.amazonaws.com'
-# export DATABASE_PORT=5432
-# export DATABASE_NAME='postgres'
