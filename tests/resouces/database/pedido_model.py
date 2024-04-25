16253bbb2be4a20ff1e5a80dfed672e378778ffc
Luiz <luiz.f_ferraresi@hotmail.com>
2024-04-24 23:12:22 -0300

    update tests resources folder

---
+++ tests/resouces/database/pedido_model.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+
+PEDIDO_MODEL_LANCHE_MOCK = {
+    'valor': 10.99,
+    'produtos': [
+        {'produto_id': 1, 'quantidade': 1}
+    ],
+    'status_pedido': 'Recebido',
+    'status_pagamento': 'Efetuado',
+    'pagamento_id': 'abc'
+}
+
+
+PEDIDO_MODEL_REFRIGERANTE_MOCK = {
+    'valor': 10.99,
+    'produtos': [
+        {'produto_id': 5, 'quantidade': 1}
+    ],
+    'status_pedido': 'Pronto',
+    'status_pagamento': 'Efetuado',
+    'pagamento_id': 'abc'
+}
+
+
+PEDIDO_MODEL_COMBO_MOCK = {
+    'valor': 21.98,
+    'produtos': [
+        {'produto_id': 1, 'quantidade': 1},
+        {'produto_id': 5, 'quantidade': 1}
+    ],
+    'status_pedido': 'Finalizado',
+    'status_pagamento': 'Efetuado',
+    'pagamento_id': 'abc'
+}
+
+PEDIDO_MODEL_COMBO_MAKING_MOCK = {
+    'valor': 21.98,
+    'produtos': [
+        {'produto_id': 1, 'quantidade': 1},
+        {'produto_id': 5, 'quantidade': 1}
+    ],
+    'status_pedido': 'Em Preparação',
+    'status_pagamento': 'Efetuado',
+    'pagamento_id': 'abc'
+}
