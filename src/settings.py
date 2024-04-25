cc68077edf835558b900dacb7bc0256844f50733
Luiz <luiz.f_ferraresi@hotmail.com>
2024-04-24 21:54:10 -0300

    update settings

--- src/settings.py
+++ src/settings.py
@@ -1,27 +1,42 @@
 from functools import lru_cache
+from typing import ClassVar
+from enum import Enum
 
 from pydantic_settings import BaseSettings
 from pydantic import Field
 from sqlalchemy.engine import URL
 
 
+class Env(str, Enum):
+    PRD = 'prd'
+    UNITTEST = 'unittest'
+
+
+def execution_environment(env: Env) -> bool:
+    return get_settings().application_settings.environment == env.value
+
+
 class ApplicationSettings(BaseSettings):
-    app_name: str = 'ByteBiteBurguers'
-    debug:   bool = True
-    app_host: str = Field(default='0.0.0.0', alias='APP_HOST', title='Application Host Address', description='')
-    app_port: int = Field(8000)
-    workers:  int = 2
+    application_name: str = Field(..., validation_alias='APPLICATION_NAME')
+    application_host: str = Field(..., validation_alias='APPLICATION_HOST')
+    application_port: int = Field(..., validation_alias='APPLICATION_PORT')
+    environment: str = Field(..., validation_alias='ENVIRONEMNT')
+    workers:  int = Field(..., validation_alias='WORKERS')
 
 
 class DatabaseSettings(BaseSettings):
-    database_username: str = Field('postgres')
-    database_password: str = Field('postgres')
-    database_host:     str = Field('localhost')
-    database_port:     int = Field(5432)
-    database_name:     str = Field('postgres')
+    database_username: str = Field(..., validation_alias='DATABASE_USERNAME')
+    database_password: str = Field(..., validation_alias='DATABASE_PASSWORD')
+    database_host:     str = Field(..., validation_alias='DATABASE_HOST')
+    database_port:     int = Field(..., validation_alias='DATABASE_PORT')
+    database_name:     str = Field(..., validation_alias='DATABASE_NAME')
 
     @property
-    def unittest_sync_uri(self) -> URL:
+    def unittest_sync_uri(self):
+        return ''
+
+    @property
+    def sync_uri(self) -> URL:
         return URL.create(
             drivername='postgresql+psycopg2',
             username=self.database_username, 
@@ -32,9 +47,9 @@ class DatabaseSettings(BaseSettings):
             )
 
 
-class GeneralSettings():
-    app_settings = ApplicationSettings()
-    database_settings = DatabaseSettings()
+class GeneralSettings(BaseSettings):
+    application_settings: ClassVar = ApplicationSettings()
+    database_settings: ClassVar = DatabaseSettings()
 
 
 @lru_cache
