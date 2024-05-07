fdd9727d282da2aeb9844e83f7eab85032621dc4
Luiz <luiz.f_ferraresi@hotmail.com>
2024-05-07 20:58:08 -0300

    add payment status

--- src/enums.py
+++ src/enums.py
@@ -1,20 +1,8 @@
 from enum import Enum
 
 
-class UsuarioTipo(Enum):
-    CLIENTE: str = 'cliente'
-    ADMIN:   str = 'admin'
-
-
-class PedidoStatus(Enum):
-    RECEBIDO:      str = 'Recebido'
-    EM_PREPARACAO: str = 'Em preparação'
-    PRONTO:        str = 'Pronto'
-    FINALIZADO:    str = 'Finalizado'
-
-
-class ProdutoCategorias(Enum):
-    LANCHE:         str = 'Lanche'
-    ACOMPANHAMENTO: str = 'Acompanhamento'
-    BEBIDA:         str = 'Bebida'
-    SOBREMESA:      str = 'Sobremesa'
+class PagamentoStatus(Enum):
+    CRIADO = 'criado'
+    APROVADO = 'aprovado'
+    NEGADO = 'negado'
+    CANCELADO = 'cancelado'
