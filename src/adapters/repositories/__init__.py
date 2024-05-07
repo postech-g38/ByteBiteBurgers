8058330e651fc9f0aa98ba2b8bf7e318e65cfa88
Luiz <luiz.f_ferraresi@hotmail.com>
2024-05-07 19:20:26 -0300

    update adapters

--- src/adapters/repositories/__init__.py
+++ src/adapters/repositories/__init__.py
@@ -1,5 +1,1 @@
-from src.adapters.repositories.pedido_repository import PedidoRepository
-from src.adapters.repositories.produto_repository import ProdutoRepository
 from src.adapters.repositories.usuario_repository import UsuarioRepository
-from src.adapters.repositories.checkout_repository import CheckoutRepository
-        
